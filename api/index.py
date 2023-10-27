69af916b5a9056a75f8dcc7f82ed29618f845855
Paul Bundi Marangu <51052499+paulbundi@users.noreply.github.com>
2023-10-27 21:06:15 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,18 +1,48 @@
-from flask import Flask
-from kivymd.app import MDApp
-from kivymd.uix.button import MDRectangleFlatButton
+from flask import Flask, render_template_string
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    class DemoApp(MDApp):
-        def build(self):
-            return MDRectangleFlatButton(text='Hello, World!')
+def plot():
+    # Define your CSS
+    css = """
+    body {
+        background-color: #f0f0f2;
+        margin: 0;
+        padding: 0;
+        font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+    }
+    div {
+        width: 600px;
+        margin: 5em auto;
+        padding: 2em;
+        background-color: #fdfdff;
+        border-radius: 0.5em;
+        box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);
+    }
+    h1 {
+        text-align: center;
+    }
+    """
 
-    DemoApp().run()
+    # Define your HTML
+    html = """
+    <html>
+    <head>
+        <style>
+            {{css}}
+        </style>
+    </head>
+    <body>
+        <div>
+            <h1>Hello, World!</h1>
+            <p>This is a simple Flask application.</p>
+        </div>
+    </body>
+    </html>
+    """
 
-    return 'Hello, World!'
+    return render_template_string(html, css=css)
 
 if __name__ == '__main__':
     app.run(debug=True)
