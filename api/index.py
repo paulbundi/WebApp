934878b8043f559b19973fd756623ae15d45a118
Paul Bundi Marangu <51052499+paulbundi@users.noreply.github.com>
2023-10-27 21:15:54 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template_string, render_template
 
 app = Flask(__name__)
 
@@ -37,12 +37,35 @@ def plot():
         <div>
             <h1>Hello, World!</h1>
             <p>This is a simple Flask application.</p>
+            <button onclick="window.location.href='/plot'">Generate Plot</button>
         </div>
+        <h1>Bar Graph</h1>
+        <img src="data:image/png;base64,{{ plot_url }}">
     </body>
     </html>
     """
 
     return render_template_string(html, css=css)
+
+
+@app.route('/plot')
+def plot():
+    # Data to plot
+    labels = ['G1', 'G2', 'G3', 'G4', 'G5']
+    men_means = [20, 35, 30, 35, 27]
+    
+    # Create bar chart
+    plt.bar(labels, men_means)
+    
+    # Convert plot to PNG image
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    
+    # Convert PNG image to base64 encoding
+    plot_url = urllib.parse.quote(base64.b64encode(img.read()).decode())
+    
+    return render_template('index.html', plot_url=plot_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
