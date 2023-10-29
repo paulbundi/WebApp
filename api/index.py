5604329bde2ca43200e43d71b43538af3e122e5c
Paul Bundi Marangu <51052499+paulbundi@users.noreply.github.com>
2023-10-29 13:36:43 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,11 +4,29 @@ import requests
 from firebase_admin import credentials, db
 from barcode import EAN13, UPCA, EAN8
 from pdf_mail import sendpdf
-from cryptography.fernet import Fernet
 from fpdf import FPDF
 import os
 from barcode.writer import ImageWriter
 from PIL import Image
+
+
+cred = credentials.Certificate({
+    "type": "service_account",
+    "project_id": "silent-owl-flight",
+    "private_key_id": "c949f25c81e713a4ca169808342f4038f5bd08de",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDO2CvYUwEZ81N3\n7BuS4IO2NwBOxg4SBSkkwAjkvqySV3QUWJeESLXdp571VZAuQnny62Dp34Ki0AYu\n3vmJ1cwuekBfoB6XhhTPflgpNJGWko1H3qRUVVrvSGhJhFsZV0KjL1ZCxo+hrfxx\nfkre6JvBvt81GbKbX3V5nMePcza8yznmCJFiw7G05VmR2Orvb0XrMoVXVm222I0c\nGM4LoEzGK3kP4nxewXqM3xiRzLMolTS//hMtezXf2MhWarhgO0Pr9WW6TjOVclrY\nkZyfTVSyxLcdd1wyzCktCvuu9cSvg06K/tB6TrxSeerH0Tfc/Y5bIXs4PEPvmKxc\n50zVu0ufAgMBAAECggEANbiFIqjkJ1Y//mxu+OWgr6l3Rf5AV0PJ3MaXSLmQ3m0i\nM8qSh4PO4CORsEwppyoSvBmP0Q4AwsJeaLv2eWrynbuYjn1qlJX5P66jvQfvK/ki\nAmF7sn98n5rhov5iHqHAJ9u6fptNYINshhlz3pLxKQ+mrwbekWXof85NMh+NPtjd\nbZNebPxs0ai+9zcuHGxGkZCMWR5QQxurCfJQOuL9YRRXWHENUOBqHrnSBMsxJnIa\nv0rzIv0Zw8M0O8CbLQwQXJLGYbz1WxHh/70tO131Ef52/DQimafkdlJkaDpxvokb\nRLfpHYamUkfEyTj05wwf9kI3fnr3CWLsoZgELd3H4QKBgQD4f+Ezk4ERxNXnLjwR\nBkmdpaH6sLGpoPgQWP5k7x0VFmGpx4inyuxRxE0X5bHLlXH2xacWy0k91wdVXxUE\n5uey6RJzYrV92k0iz0FeIliG1J3CenY+9TBpy0BhKD3wU0Z6U+JG7lMZhdPO6xSL\ntQvtKTCTKYObJ7Q5Mem493mO0QKBgQDVFm2x0a8ZEhKn8rA319CesYlp8YTf+pCb\nPUzDPpsB13BIpUdRyeBVkjsWwdXHzNffXmg3LUPXJLu7JRrKHV3kmGohCM+D30iT\n1SqRl6KDLbyyVEwEgKpZkkSwecVKfJdnffLF5ZDxVzK/yTf4dINqJFtlJtqgn6zk\nFRExiW4vbwKBgQCHYlxWFiG2hY+oELdm6w8GVWvnQYa5jNo5RdLrOJ6Wk4gH0I6y\nI8sWTSVXRKvV54icUljTAVPY3iK/rzqrXgWToomL1ZZdh6aItXO+jmW+p21/u+pa\nKIkEylg71onQOf5mvPbFgChD+nZIAClEaBGkVtGBC5gI2tvEYV4diK5wUQKBgA2f\nl3fM7iLuMt8l9vo7BA2BUEXDuTkQrfoe2y+IaySc/4LFfl/ORldyN/Dmh6J0iECx\nqnwMms/Ae3glkSbm5b/dtrtR8uJ9fghlkokoZq14WR4VoZ6QE1QSs+2Me36dpJhr\nRwA8Ax5K8uWsGbX6zQQWOvmmmEe4rbcPWcAPB0plAoGAFhI5zBP7PLTMmuYIaFnl\nIbdO0Yc27ABkaFHxqrVexJzClqQ64KiSiJv4yb+gO0ux3kbpE6grt2/YlJkCjdzI\nh323NvVto1Fo+CjtAwyWH7+DlGGk4ThQM62JmXk9NcG4tS1xOZrkA4zakaWBtz35\n4q+b0suRRrNNZ+u0ruFlLxI=\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-q168e@silent-owl-flight.iam.gserviceaccount.com",
+    "client_id": "113686962064434702925",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-q168e%40silent-owl-flight.iam.gserviceaccount.com"
+})
+firebase_admin.initialize_app(cred, {
+    "databaseURL": "https://silent-owl-flight-default-rtdb.europe-west1.firebasedatabase.app",
+    'storageBucket': "silent-owl-flight.appspot.com"
+})
+database = db
 
 app = Flask(__name__)
 
